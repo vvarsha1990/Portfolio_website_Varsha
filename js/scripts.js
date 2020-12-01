9502ccaaafe2bbc7603b71d61fb9bb6ce25c2e07
vvarsha1990 <71457002+vvarsha1990@users.noreply.github.com>
2020-12-01 12:59:58 -0600

    Changes to jquery
    Made changes to jquery

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,10 @@
-console.log ("Hello! Welcome to my Portfolio website.");
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+     $(".navtoggle.responsive nav").fadeIn(1000);
+  } else {
+    x.className = 'navtoggle';
+     $(".navtoggle nav").attr({style: ''});
+  }
+}
